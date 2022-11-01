0f459bb113e23f3ababe73ff3e98bfcb75795660
XFFXFF <1247714429@qq.com>
2022-11-01 20:24:30 +0800

    use flake

--- configuration.nix
+++ configuration.nix
@@ -187,4 +187,9 @@
   system.stateVersion = "22.05"; # Did you read the comment?
 
   nix.trustedUsers = [ "root" "xffxff" ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
 }
